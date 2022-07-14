782c90a303eecf4283f2f6f4f4e7a653e0ac6a92
Max <7087907+maxpromer@users.noreply.github.com>
2022-07-15 03:03:30 +0700

    Add force reconnect by WiFi event

--- src/WiFiManager.cpp
+++ src/WiFiManager.cpp
@@ -3,22 +3,44 @@
 #include "./StorageConfigs.h"
 #include "./Board/Board.h"
 
+static bool wifi_force_disconnect = false;
+
+static void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+        case SYSTEM_EVENT_STA_STOP:
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+        case SYSTEM_EVENT_STA_LOST_IP:
+            Serial.println("WiFi Disconnect");
+            wifi_force_disconnect = true;
+            break;
+        default:
+            break;
+    }
+}
+
 static int state = 0;
 
 void WiFiManager_process() {
     if (state == 0) {
+        WiFi.onEvent(WiFiEvent);
+    } else if (state == 1) {
         if (!WiFi.isConnected()) {
             if (GlobalConfigs.containsKey("wifi") && GlobalConfigs["wifi"].containsKey("ssid") && GlobalConfigs["wifi"].containsKey("password")) {
                 WiFi.begin(
                     GlobalConfigs["wifi"]["ssid"].as<const char*>(),
                     GlobalConfigs["wifi"]["password"].as<const char*>()
                 );
-                state = 1;
+                state = 2;
             }
         }
-    } else if (state == 1) {
-        if (WiFi.isConnected()) {
-            state = 0;
+    } else if (state == 2) {
+        if (WiFi.isConnected() || wifi_force_disconnect) {
+            if (wifi_force_disconnect) {
+                WiFi.disconnect();
+
+                wifi_force_disconnect = false;
+            }
+            state = 1;
         } else {
             // LED Blink status
             static uint64_t last_led_blink = 0;
