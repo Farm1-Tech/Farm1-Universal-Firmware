3598c3875d053f3442f4c0f69820b478df2f56c1
Max <7087907+maxpromer@users.noreply.github.com>
2022-07-17 20:23:05 +0700

    Fixed Farm1-LCD3.2 not work

--- lib/TFT_eSPI-2.4.61/User_Setups/Setup1_ILI9341.h
+++ lib/TFT_eSPI-2.4.61/User_Setups/Setup1_ILI9341.h
@@ -4,10 +4,11 @@
 #define ILI9341_DRIVER
 
 
-#define TFT_CS   PIN_D8  // Chip select control pin D8
-#define TFT_DC   PIN_D3  // Data Command control pin
-#define TFT_RST  PIN_D4  // Reset pin (could connect to NodeMCU RST, see next line)
+#define TFT_CS   5   // Chip select control pin D8
+#define TFT_DC   17  // Data Command control pin
+#define TFT_RST  27  // Reset pin (could connect to NodeMCU RST, see next line)
 //#define TFT_RST  -1    // Set TFT_RST to -1 if the display RESET is connected to NodeMCU RST or 3.3V
+#define TOUCH_CS 16     // Chip select pin (T_CS) of touch screen
 
 
 #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
