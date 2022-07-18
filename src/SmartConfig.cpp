6b5c0fc182a783b4d805a9881cfd9a7199ab628d
Max <7087907+maxpromer@users.noreply.github.com>
2022-07-19 01:54:02 +0700

    Add wait wifi connected before exit SmartConfig

--- src/SmartConfig.cpp
+++ src/SmartConfig.cpp
@@ -5,7 +5,10 @@
 #include "./StorageConfigs.h"
 #include "./Board/Board.h"
 
+#define WAIT_WIFI_CONNECT_TIMEOUT (30 * 1000)
+
 static int state = 0;
+static uint64_t start_wait_wifi_connected = 0;
 
 SmartConfigStatus_t SmartConfig_process() {
     if (state == 0) {
@@ -14,34 +17,40 @@ SmartConfigStatus_t SmartConfig_process() {
             GlobalConfigs["wifi"].clear();
         }*/
 
+        Serial.println("SmartConfig begin");
         WiFi.beginSmartConfig();
 
         state = 1;
     } else if (state == 1) {
         if (WiFi.smartConfigDone()) {
             Serial.println("SmartConfig received.");
-
+            start_wait_wifi_connected = millis();
+            state = 2;
+        }
+    } else if (state == 2) {
+        if (WiFi.isConnected()) {
             Serial.print("SSID: ");
             Serial.println(WiFi.SSID());
             Serial.print("Password: ");
             Serial.println(WiFi.psk());
 
-            /*
             // Save configs
-            GlobalConfigs["wifi"]["ssid"] = "";
+            GlobalConfigs["wifi"]["ssid"] = WiFi.SSID();
             GlobalConfigs["wifi"]["password"] = WiFi.psk();
-            // serializeJson(GlobalConfigs, Serial);
+            serializeJson(GlobalConfigs, Serial);
 
             // Save Configs
             if (!StorageConfigs_save()) {
                 Serial.println("Save configs fail !");
             }
-            */
 
-            GlobalConfigs["wifi"]["ssid"] = "";
+            //GlobalConfigs["wifi"]["ssid"] = "";
             
             state = 0;
             return FINISH_CONFIG;
+        } else if ((millis() - start_wait_wifi_connected) > WAIT_WIFI_CONNECT_TIMEOUT) {
+            Serial.println("Wait WiFi connnect timeout");
+            state = 0;
         }
     }
 
