706ce91fc8333f4f1b29b5d631974df8aadf94e9
Max <7087907+maxpromer@users.noreply.github.com>
2022-07-20 18:08:33 +0700

    Add AM2301

--- src/Sensor/Sensor.h
+++ src/Sensor/Sensor.h
@@ -7,6 +7,7 @@
 #include "./SHT30/SHT30.h"
 #include "./XY-MD02/XY-MD02.h"
 #include "./DHT22/DHT22.h"
+#include "./AM2301/AM2301.h"
 #include "./DS18B20/DS18B20.h"
 #include "./AnalogSoil/AnalogSoil.h"
 #include "./JXBS-3001-TR/JXBS-3001-TR.h"
@@ -19,6 +20,7 @@ const Sensor_t Sensor[] = {
     SHT30_Sensor,
     XY_MD02_Sensor,
     DHT22_Sensor,
+    AM2301_Sensor,
     DS18B20_Sensor,
 
     AnalogSoil_Sensor,
