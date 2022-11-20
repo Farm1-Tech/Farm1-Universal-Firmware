d38402004c1ec5a8173f524c1a53614196cb8262
Max <7087907+maxpromer@users.noreply.github.com>
2022-11-21 06:07:20 +0700

    Use ESP_LOGx for show debug

--- src/StorageConfigs.cpp
+++ src/StorageConfigs.cpp
@@ -3,6 +3,7 @@
 #include "./StorageConfigs.h"
 
 #define GLOBAL_FILE_CONFIGS "/configs.json"
+static const char* TAG = "StorageConfigs";
 
 // StaticJsonDocument <CONFIG_BUFFER_SIZE>GlobalConfigs;
 DynamicJsonDocument *GlobalConfigsJsonDoc;
@@ -14,7 +15,7 @@ void StorageConfigs_init() {
 static bool fileWrite(String fileName, String content) {
   File file = SPIFFS.open(fileName, FILE_WRITE);
   if (!file) {
-    Serial.printf("failed to open file '/%s' for writing\n", fileName.c_str());
+    ESP_LOGE(TAG, "failed to open file '/%s' for writing\n", fileName.c_str());
     return false;
   }
   file.print(content);
@@ -26,7 +27,7 @@ static bool fileWrite(String fileName, String content) {
 static String fileRead(String fileName) {
   File file = SPIFFS.open(fileName, FILE_READ);
   if (!file) {
-    Serial.printf("failed to open file '/%s' for reading\n", fileName.c_str());
+    ESP_LOGE(TAG, "failed to open file '/%s' for reading\n", fileName.c_str());
     return String("");
   }
   String content = file.readString();
@@ -40,7 +41,7 @@ bool StorageConfigs_load() {
     // Serial.println(configsContentText);
     GlobalConfigs.clear();
     if (deserializeJson(GlobalConfigs, configsContentText) != DeserializationError::Ok) {
-        Serial.println("Load configs error");
+        ESP_LOGE(TAG, "Load configs error");
         return false;
     }
 
